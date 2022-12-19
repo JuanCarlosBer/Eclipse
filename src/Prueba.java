73ff8dcb95e8945e05c541fdcde12e400246e3f1
JUANCARLOS <JCBAPRO82@GMAIL.COM>
2022-12-19 09:43:26 +0100

    Añadiendo String

--- src/Prueba.java
+++ src/Prueba.java
@@ -3,6 +3,10 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("JuanCarlos");
+		
+		String nombre = "JuanCarlos";
+		System.out.println(nombre);
+
 	}
 
 }
